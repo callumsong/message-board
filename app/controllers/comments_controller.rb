8b899c339694070129893fbff07abb9f83a45b2e
callumsong <ghksong@gmail.com>
2016-01-31 18:03:42 +0000

    comments can be deleted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  before_action :set_comment, only: [:show, :destroy]
+
   def index
     @message = Message.find(params[:message_id])
     @comments = @message.comments
@@ -11,7 +13,22 @@ class CommentsController < ApplicationController
     redirect_to messages_path
   end
 
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to messages_url, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def show
+  end
+
   private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
     def comment_params
       params.require(:comment).permit(:comment_text, :message_id)
     end
